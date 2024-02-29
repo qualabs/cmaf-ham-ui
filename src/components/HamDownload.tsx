9b2064b7b58462add223e8e20b14551925a6cd97
Constanza Dibueno <constanzad@qualabs.com>
2024-02-29 18:09:03 -0300

    separate download methods to avoid too many conditionals

--- src/components/HamDownload.tsx
+++ src/components/HamDownload.tsx
@@ -1,29 +1,37 @@
-import React from "react";
 import * as Ham from "@svta/common-media-library";
 
 export const HamDownload = ({presentation}: { presentation: Ham.Presentation, protocol: string }) => {
 
-  const downloadTMpdFile = async (event: React.MouseEvent<HTMLButtonElement>, protocol: string) => {
-    let manifest;
-    if(protocol == 'dash'){
-      manifest = await Ham.hamToMpd(presentation);
-    } else if (protocol == 'hls') {
-      // manifest = await Ham.hamToM3U8(presentation);
-    }
+  const downloadMpdFile = async () => {
+    let manifest = await Ham.hamToMpd(presentation);
     if (manifest){
       const element = document.createElement("a");
-      const file = new Blob([manifest], {type: 'text/plain'})
-      element.href = URL.createObjectURL(file);
-      element.download = "myFile.txt";
-      document.body.appendChild(element);
-      element.click();
+      const file = new Blob([manifest], {type: 'application/dash+xml'})
+      element.download = "myFile.mpd"; //TODO access original filename
+      triggerDownloadLink(element, file);
     }
+  }
+
+  const downloadUM38File = async () => {
+    // let manifest = await Ham.hamToM3U8(presentation);
+    // if (manifest){
+    //   const element = document.createElement("a");
+    //   const file = new Blob([manifest], {type: 'application/x-mpegURL'})
+    //   element.download = "myFile.m3u8"; //TODO access original filename
+    //   triggerDownloadLink(element, file);
+    // }
+  }
+
+  function triggerDownloadLink(element: HTMLAnchorElement, file: Blob){
+    element.href = URL.createObjectURL(file);
+    document.body.appendChild(element);
+    element.click();
   }
 
   return (
     <>
-      <button onClick={(e) => downloadTMpdFile(e, 'dash')}>Download as DASH</button>
-      <button onClick={(e) => downloadTMpdFile(e, 'hls')}>Download as HLS</button>
+      <button onClick={downloadMpdFile}>Download as DASH</button>
+      <button onClick={downloadUM38File}>Download as HLS</button>
     </>
   )
 }
