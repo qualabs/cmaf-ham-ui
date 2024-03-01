4fbc84e7746c56ea8801feb9289f26f1b523f21a
Constanza Dibueno <constanzad@qualabs.com>
2024-03-01 09:57:26 -0300

    refactor ham display

--- src/components/HamDisplay.tsx
+++ src/components/HamDisplay.tsx
@@ -4,24 +4,38 @@ import Presentation from "./ham/Presentation";
 import { Container } from "@mui/material";
 import HamDownload from "./HamDownload";
 
+enum Protocols {
+  HLS= "hls",
+  DASH= "dash"
+}
+
 export const HamDisplay = ({ manifest, protocol }: { manifest: string, protocol: string }) => {
   const [presentation, setPresentation] = useState<Ham.Presentation | null>(null);
 
   useEffect(() => {
     const mapManifest = async function (manifest: string) {
-      console.log(manifest);
-      console.log(protocol);
-      if (protocol == "dash"){
-        return Ham.mpdToHam(manifest)
-          .then(setPresentation)
-          .catch((e) => {
-            console.error("Erorr while parsing manifest", e);
-          }
-        );
-      }
-      if(protocol == "hsl"){
-        //hlsToHam
-      }
+      // console.log(manifest);
+      // console.log(protocol);
+      switch(protocol) {
+        case Protocols.DASH:
+          return Ham.mpdToHam(manifest)
+            .then(setPresentation)
+            .catch((e) => {
+              console.error("Erorr while parsing manifest", e);
+            }
+          );
+          break;
+        case Protocols.HLS:
+          // return Ham.mpdToM3U8(manifest)
+          //   .then(setPresentation)
+          //   .catch((e) => {
+          //     console.error("Erorr while parsing manifest", e);
+          //   }
+          // );
+          break;
+        default:
+          break;
+        }
     };
 
     mapManifest(manifest);
