4e9da70ad8921ffcfab22388205ad7be8d7c8c0d
montanaeli <montanaeli@hotmail.com>
2024-06-24 16:07:18 -0300

    Added tooltip on selection and switching sets

--- src/components/ham/SelectionSet.tsx
+++ src/components/ham/SelectionSet.tsx
@@ -2,6 +2,7 @@ import "./SelectionSet.css";
 import * as Ham from "@svta/common-media-library/cmaf-ham";
 import SwitchingSet from "./SwitchingSet";
 import { Grid, Box } from "@mui/material";
+import ToolTip from "../ToolTip";
 
 export default function SelectionSet({
   selectionSet,
@@ -18,7 +19,16 @@ export default function SelectionSet({
 
   return (
     <div className="selection-set-card">
-      <h4>Selection Set {selectionSet.id}</h4>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="row"
+        gap={1}
+      >
+        <h4>Selection Set {selectionSet.id}</h4>
+        <ToolTip title="Example info"></ToolTip>
+      </Box>
       <Box sx={{ flexGrow: 1 }}>
         <Grid
           container
--- src/components/ham/SwitchingSet.tsx
+++ src/components/ham/SwitchingSet.tsx
@@ -1,7 +1,8 @@
 import "./SwitchingSet.css";
 import * as Ham from "@svta/common-media-library/cmaf-ham";
 import Track from "./Track";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import ToolTip from "../ToolTip";
 
 export default function SwitchingSet({
   switchingSet,
@@ -10,21 +11,33 @@ export default function SwitchingSet({
 }) {
   let tracks = switchingSet.tracks.map((track: Ham.Track, index) => (
     <Grid item xs={4} key={index}>
-      <Track track={track} key={`track-item-${track.id}`}/>
+      <Track track={track} key={`track-item-${track.id}`} />
     </Grid>
   ));
   return (
-    <div className="switching-set-card" id={`switching-set-card-${switchingSet.id}`}>
+    <div
+      className="switching-set-card"
+      id={`switching-set-card-${switchingSet.id}`}
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="row"
+        gap={1}
+      >
         <h5>Switching Set: {switchingSet.id}</h5>
-        <Grid
-          container
-          direction="column"
-          justifyContent="space-evenly"
-          alignItems="stretch"
-          gap={2}
-        >
-          {tracks}
-        </Grid>
+        <ToolTip title="Example info"></ToolTip>
+      </Box>
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="stretch"
+        gap={2}
+      >
+        {tracks}
+      </Grid>
     </div>
   );
 }
